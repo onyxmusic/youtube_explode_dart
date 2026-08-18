f0c6753222204cde8da173cecbf7da1658a83bc6
onyxmusic <faruk-ocal@hotmail.com>
2026-08-18 14:45:19 +0300

    Update stream_client.dart

--- lib/src/videos/streams/stream_client.dart
+++ lib/src/videos/streams/stream_client.dart
@@ -105,12 +105,7 @@ class StreamClient {
             );
           }
 
-          final response = await _httpClient.head(streams.first.url);
-          if (response.statusCode == 403) {
-            throw YoutubeExplodeException(
-              'Video $videoId returned 403 (stream: ${streams.first.tag})',
-            );
-          }
+          
           uniqueStreams.addAll(streams);
         });
       } catch (e, s) {
