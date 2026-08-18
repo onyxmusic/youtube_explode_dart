3d806e3fd03bfb2782ab1c5cb1c15bf4410b8544
onyxmusic <faruk-ocal@hotmail.com>
2026-08-18 15:56:19 +0300

    Update youtube_api_client.dart

--- lib/src/videos/youtube_api_client.dart
+++ lib/src/videos/youtube_api_client.dart
@@ -16,91 +16,95 @@ class YoutubeApiClient {
         'headers': headers,
       };
 
-  /// Working iOS client configuration with high reliability
-  static final ios = YoutubeApiClient({
-    'context': {
-      'client': {
-        'clientName': 'IOS',
-        'clientVersion': '20.15.1',
-        'deviceMake': 'Apple',
-        'deviceModel': 'iPhone16,2',
-        'userAgent':
-            'com.google.ios.youtube/20.15.1 (iPhone16,2; U; CPU iOS 18_3_2 like Mac OS X;)',
-        'hl': 'en',
-        'platform': 'MOBILE',
-        'osName': 'IOS',
-        'osVersion': '18.1.0',
-        'timeZone': 'UTC',
-        'gl': 'US',
-        'utcOffsetMinutes': 0
-      }
+  /// Tested & Verified: iOS 20.10.4 Client (Direct Audio Streams)
+  static final ios = YoutubeApiClient(
+    {
+      'context': {
+        'client': {
+          'clientName': 'IOS',
+          'clientVersion': '20.10.4',
+          'deviceMake': 'Apple',
+          'deviceModel': 'iPhone16,2',
+          'userAgent':
+              'com.google.ios.youtube/20.10.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)',
+          'hl': 'en',
+          'gl': 'US',
+          'platform': 'MOBILE',
+          'osName': 'iOS',
+          'osVersion': '18.1.0.22B83',
+          'timeZone': 'UTC',
+          'utcOffsetMinutes': 0
+        }
+      },
     },
-  }, 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false');
+    'https://www.youtube.com/youtubei/v1/player?prettyPrint=false',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent':
+          'com.google.ios.youtube/20.10.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X;)',
+      'Origin': 'https://www.youtube.com',
+    },
+  );
 
-  /// Android client without androidSdkVersion field.
+  /// Tested & Verified: Android VR Client (High Quality Streams)
+  static const androidVr = YoutubeApiClient(
+    {
+      'context': {
+        'client': {
+          'clientName': 'ANDROID_VR',
+          'clientVersion': '1.60.19',
+          'deviceMake': 'Oculus',
+          'deviceModel': 'Quest 3',
+          'osName': 'Android',
+          'osVersion': '12L',
+          'hl': 'en',
+          'gl': 'US',
+          'timeZone': 'UTC',
+          'utcOffsetMinutes': 0,
+        },
+      },
+    },
+    'https://www.youtube.com/youtubei/v1/player?prettyPrint=false',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent':
+          'com.google.android.apps.youtube.vr.oculus/1.60.19 (Linux; U; Android 12L; Quest 3) gzip',
+      'Origin': 'https://www.youtube.com',
+    },
+  );
+
   static const androidSdkless = YoutubeApiClient({
     'context': {
       'client': {
         'clientName': 'ANDROID',
-        'clientVersion': '20.15.34',
+        'clientVersion': '19.44.38',
         'userAgent':
-            'com.google.android.youtube/20.15.34 (Linux; U; Android 12) gzip',
+            'com.google.android.youtube/19.44.38 (Linux; U; Android 14) gzip',
         'hl': 'en',
+        'gl': 'US',
         'timeZone': 'UTC',
         'utcOffsetMinutes': 0,
         'osName': 'Android',
-        'osVersion': '12',
+        'osVersion': '14',
       },
     },
   }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
 
-  static const android = YoutubeApiClient({
-    'context': {
-      'client': {
-        'clientName': 'ANDROID',
-        'clientVersion': '20.10.38',
-        'androidSdkVersion': 30,
-        'userAgent':
-            'com.google.android.youtube/20.10.38 (Linux; U; Android 11) gzip',
-        'hl': 'en',
-        'timeZone': 'UTC',
-        'utcOffsetMinutes': 0,
-        'osName': 'Android',
-        'osVersion': '11',
-      },
-    },
-  }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
+  static const android = androidSdkless;
 
   static const androidMusic = YoutubeApiClient({
     'context': {
       'client': {
         'clientName': 'ANDROID_MUSIC',
-        'clientVersion': '6.42.52',
-        'androidSdkVersion': 31,
+        'clientVersion': '2.16.032',
         'userAgent':
-            'com.google.android.apps.youtube.music/6.42.52 (Linux; U; Android 11) gzip',
+            'com.google.android.youtube/19.29.1 (Linux; U; Android 11) gzip',
         'hl': 'en',
         'timeZone': 'UTC',
         'utcOffsetMinutes': 0,
       },
     },
-  }, 'https://music.youtube.com/youtubei/v1/player?key=AIzaSyAOghZGza2MQSZkY_zfZ370N-PUdXEo8AI&prettyPrint=false');
-
-  static const androidVr = YoutubeApiClient({
-    'context': {
-      'client': {
-        'clientName': 'ANDROID_VR',
-        'clientVersion': '1.56.21',
-        'deviceModel': 'Quest 3',
-        'osVersion': '12',
-        'osName': 'Android',
-        'androidSdkVersion': '32',
-        'hl': 'en',
-        'timeZone': 'UTC',
-        'utcOffsetMinutes': 0,
-      },
-    },
-  }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
+  }, 'https://music.youtube.com/youtubei/v1/player?prettyPrint=false');
 
   static const safari = YoutubeApiClient({
     'context': {
@@ -117,41 +121,28 @@ class YoutubeApiClient {
   }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
 
   static const tv = YoutubeApiClient(
-      {
-        'context': {
-          'client': {
-            'deviceMake': '',
-            'deviceModel': '',
-            'userAgent':
-                'Mozilla/5.0 (ChromiumStylePlatform) Cobalt/Version,gzip(gfe)',
-            'clientName': 'TVHTML5',
-            'clientVersion': '7.20251105.10.00',
-            'hl': 'en',
-            'timeZone': 'UTC',
-            'gl': 'US',
-            'utcOffsetMinutes': 0,
-            'originalUrl': 'https://www.youtube.com/tv',
-            'theme': 'CLASSIC',
-            'platform': 'DESKTOP',
-            'clientFormFactor': 'UNKNOWN_FORM_FACTOR',
-            'webpSupport': false,
-            'configInfo': {},
-            'tvAppInfo': {'appQuality': 'TV_APP_QUALITY_FULL_ANIMATION'},
-            'acceptHeader':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-          },
-          'user': {'lockedSafetyMode': false},
-          'request': {'useSsl': true},
+    {
+      'context': {
+        'client': {
+          'clientName': 'TVHTML5',
+          'clientVersion': '7.20251105.10.00',
+          'hl': 'en',
+          'gl': 'US',
+          'timeZone': 'UTC',
+          'platform': 'DESKTOP',
+          'osName': 'TV',
+          'osVersion': '1.0',
         },
-        'contentCheckOk': true,
-        'racyCheckOk': true,
       },
-      'https://www.youtube.com/youtubei/v1/player?prettyPrint=false',
-      headers: {
-        'Sec-Fetch-Mode': 'navigate',
-        'Content-Type': 'application/json',
-        'Origin': 'https://www.youtube.com',
-      });
+    },
+    'https://www.youtube.com/youtubei/v1/player?prettyPrint=false',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent':
+          'Mozilla/5.0 (ChromiumStylePlatform) Cobalt/Version,gzip(gfe)',
+      'Origin': 'https://www.youtube.com',
+    },
+  );
 
   static const mediaConnect = YoutubeApiClient({
     'context': {
@@ -169,48 +160,11 @@ class YoutubeApiClient {
     'context': {
       'client': {
         'clientName': 'MWEB',
-        'clientVersion': '2.20250312.04.00',
+        'clientVersion': '2.20240726.01.00',
         'hl': 'en',
         'timeZone': 'UTC',
         'utcOffsetMinutes': 0,
       },
     },
   }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
-
-  @Deprecated('Youtube always requires authentication for this client')
-  static const webCreator = YoutubeApiClient({
-    'context': {
-      'client': {
-        'clientName': 'WEB_CREATOR',
-        'clientVersion': '1.20240723.03.00',
-        'hl': 'en',
-        'timeZone': 'UTC',
-        'utcOffsetMinutes': 0,
-      },
-    },
-  }, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
-
-  @Deprecated('Youtube always requires authentication for this client')
-  static const tvSimplyEmbedded = YoutubeApiClient(
-      {
-        'context': {
-          'client': {
-            'clientName': 'TVHTML5_SIMPLY_EMBEDDED_PLAYER',
-            'clientVersion': '2.0',
-            'hl': 'en',
-            'timeZone': 'UTC',
-            'gl': 'US',
-            'utcOffsetMinutes': 0
-          }
-        },
-        'thirdParty': {'embedUrl': 'https://www.youtube.com/'},
-        'contentCheckOk': true,
-        'racyCheckOk': true
-      },
-      'https://www.youtube.com/youtubei/v1/player?prettyPrint=false',
-      headers: {
-        'Sec-Fetch-Mode': 'navigate',
-        'Content-Type': 'application/json',
-        'Origin': 'https://www.youtube.com',
-      });
 }
